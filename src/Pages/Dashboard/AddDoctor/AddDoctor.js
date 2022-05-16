fc7847004f76b4dbe39e1a48d7ae844a99a18d7c
Hossen <mdmitho199911@gmail.com>
2022-05-16 23:24:13 +0600

    Load services with data project and Add file type input

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { useQuery } from 'react-query';
+import Loading from '../../Shared/Loading'
 
 const AddDoctor = () => {
     const {
@@ -8,6 +10,13 @@ const AddDoctor = () => {
       handleSubmit,
     } = useForm();
 
+const { data:services,isLoading } = useQuery("services", () =>
+  fetch("http://localhost:5000/service").then((res) => res.json())
+);
+
+if(isLoading){
+  return <Loading></Loading>
+}
 
     const onSubmit = async (data) => {
 
@@ -76,27 +85,34 @@ const AddDoctor = () => {
             <label className="label">
               <span className="label-text">Specialization</span>
             </label>
+
+            <select {...register("specialty")} class="select w-full max-w-xs">
+              {services.map((service) => (
+                <option key={service._id} value={service.name}>
+                  {service.name}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Photo</span>
+            </label>
             <input
-              type="text"
-              placeholder="specialty"
+              type="file"
+             
               className="input input-bordered w-full max-w-xs"
-              {...register("specialty", {
+              {...register("image", {
                 required: {
                   value: true,
-                  message: "Specialization is Required",
-                },
-                minLength: {
-                  value: 6,
-                  message: "Must be 6 Characters or longer",
+                  message: "Image is Required",
                 },
               })}
             />
             <label className="label">
-              {errors.password?.type === "required" && (
-                <span className="label-text-alt text-red-500">{errors.password.message}</span>
-              )}
-              {errors.password?.type === "minLength" && (
-                <span className="label-text-alt text-red-500">{errors.password.message}</span>
+              {errors.name?.type === "required" && (
+                <span className="label-text-alt text-red-500">{errors.name.message}</span>
               )}
             </label>
           </div>
